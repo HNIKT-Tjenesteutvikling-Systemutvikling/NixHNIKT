635c973c0825e2c4cf81c43741437683897a8260
Gako358 <gako358@outlook.com>
2023-07-31 18:02:33 +0200

    fix gtk options

--- home/common/services/default.nix
+++ home/common/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./gtk.nix
     ./shell.nix
   ];
 }
--- update.sh
+++ update.sh
@@ -3,31 +3,13 @@
 set -ex
 
 echo "Updating system..."
-# Comment lines 20 and 21 in .gitignore
-echo "Commenting lines 20 and 21 in .gitignore"
-sed -i '21,22s/^/#/' .gitignore
-
-# Stage the specified folder
-echo "Staging the specified folder"
-git add modules/nixos/programs/certs
-
 git pull
 
 # Run the nixos-rebuild command
 sudo nixos-rebuild switch --flake .#hnikt
 
 # Clean up
-echo "Cleaning up"
-sed -i '21,22s/^#//' .gitignore
-git reset modules/nixos/programs/certs
-
-echo "System updated!, please enter the hostname:"
-read user
+echo "System updated!, updating home-manager..."
 
 # Update home-manager
 home-manager switch --flake .#"dev@hnikt"
-
-# Commit and push
-git add .
-git commit -m "System update carried out on $(date)"
-git push
