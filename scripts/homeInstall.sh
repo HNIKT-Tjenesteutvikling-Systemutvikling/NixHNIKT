53ba44230fe6be4e73ca1da050a400d6a2502a9e
merrinx <gako358@outlook.com>
2023-08-08 07:54:31 +0200

    fix update script to follow new user

---
+++ scripts/homeInstall.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Check if profile exists
+if [ -d ~/.local/state/nix/profiles ]; then
+    echo "Profile exists"
+else
+    echo "Profile does not exist"
+    mkdir -p ~/.local/state/nix/profiles
+fi
+
+# Get flake input from the user
+clear
+echo "Please enter the flake:"
+echo "1. hnikt, 2. grindstein, 3. solheim, 4. sigubrat, 5. turbonaepskrel"
+
+read flake
+# Set the flake
+if [[ "$flake" == "1" ]]
+then
+    flake="hnikt"
+elif [[ "$flake" == "2" ]]
+then
+    flake="grindstein"
+elif [[ "$flake" == "3" ]]
+then
+    flake="solheim"
+elif [[ "$flake" == "4" ]]
+then
+    flake="sigubrat"
+elif [[ "$flake" == "5" ]]
+then
+    flake="turbonaepskrel"
+else
+    echo "Invalid input!"
+    echo "Use 1, 2, 3 or 4"
+    exit 1
+fi
+home_name="dev@$flake"
+
+# Install Home-manager
+nix run github:nix-community/home-manager#home-manager -- switch --flake .#$home_name;
