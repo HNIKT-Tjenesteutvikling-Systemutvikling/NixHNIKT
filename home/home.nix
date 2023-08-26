ba2e34f2d16963a0615e0af3d2d837e57a6a1e34
merrinx <gako358@outlook.com>
2023-08-26 13:56:12 +0200

    fix printing and setup emacs (basic)

--- home/home.nix
+++ home/home.nix
@@ -33,6 +33,7 @@
     ouch                 # painless compression and decompression for your terminal
     paprefs              # pulseaudio preferences
     prettyping           # a nicer ping
+    postman              # api testing
     rage                 # encryption tool for secrets management
     spotify              # music streaming
     ripgrep              # fast grep
