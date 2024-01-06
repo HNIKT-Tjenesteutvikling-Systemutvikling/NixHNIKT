ea8aa6b71bc55b11c68deeee4e8825a590bcb4e4
Knut Øien <gako358@outlook.com>
2024-01-06 13:07:55 +0100

    Update home manager install script

--- scripts/homeInstall.sh
+++ scripts/homeInstall.sh
@@ -8,34 +8,15 @@ else
     mkdir -p ~/.local/state/nix/profiles
 fi
 
-# Get flake input from the user
-clear
-echo "Please enter the flake:"
-echo "1. grindstein, 2. jca, 3. solheim, 4. sigubrat, 5. turbonaepskrel"
+git add -f userSetting.nix
 
-read flake
-# Set the flake
-if [[ "$flake" == "1" ]]
-then
-    flake="grindstein"
-elif [[ "$flake" == "2" ]]
-then
-    flake="jca"
-elif [[ "$flake" == "3" ]]
-then
-    flake="solheim"
-elif [[ "$flake" == "4" ]]
-then
-    flake="sigubrat"
-elif [[ "$flake" == "5" ]]
-then
-    flake="turbonaepskrel"
-else
-    echo "Invalid input!"
-    echo "Use 1, 2, 3 or 4 or 5"
-    exit 1
-fi
+# Extract the username from the userSetting.nix file
+flake=$(grep 'hostname' userSetting.nix | cut -d '"' -f 2)
 home_name="dev@$flake"
 
 # Install Home-manager
 nix run github:nix-community/home-manager#home-manager -- switch --flake .#$home_name;
+
+git reset userSetting.nix
+
+echo "Home-manager installed"
