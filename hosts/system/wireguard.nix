48b7e6469cc6557cd7c20f03e89779d362d9e47f
merrinx <gako358@outlook.com>
2024-01-11 10:28:42 +0100

    Africa.....

--- hosts/system/wireguard.nix
+++ hosts/system/wireguard.nix
@@ -1,21 +1,12 @@
 { pkgs, ... }: {
-  networking.wg-quick.interfaces =
+  networking.wireguard.interfaces =
     let
       server_ip = "176.125.235.71";
     in
     {
       wg0 = {
-        # IP address of this machine in the *tunnel network*
-        address = [
-          "10.64.186.60/32"
-          "fdc9:281f:04d7:9ee9::2/64"
-        ];
-
-        # To match firewall allowedUDPPorts (without this wg
-        # uses random port numbers).
+        ips = [10.66.203.155/32];
         listenPort = 51820;
-
-        # Path to the private key file.
         privateKeyFile = "/etc/mullvad-vpn.key";
 
         peers = [{
