eb5c1e177531e072a1968af4ecc3c201983730c0
ievensen <idar.evensen@gmail.com>
2024-06-06 14:21:21 +0200

    only god knows

--- hosts/users/ievensen/default.nix
+++ hosts/users/ievensen/default.nix
@@ -4,13 +4,28 @@
   ...
 }: {
   desktop.environment = "gnome";
+  environment.systemPackages = [ pkgs.k3s ];
   networking.hostName = "ievensen";
-  services.xserver = {
-    videoDrivers = ["intel"]; # Optional use displayLink for USB-C docking station
-    displayManager = {
-      sessionCommands = ''
-        ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-      '';
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s default API server port
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+  services = {
+    k3s.enable = true;
+    k3s.role = "server";
+    k3s.extraFlags = toString [
+      "--write-kubeconfig-mode 644"
+      "--node-name k3s-idev-master-01"
+    ];
+    xserver = {
+      videoDrivers = ["intel"]; # Optional use displayLink for USB-C docking station
+      displayManager = {
+        sessionCommands = ''
+          ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
+        '';
+      };
     };
   };
 }
