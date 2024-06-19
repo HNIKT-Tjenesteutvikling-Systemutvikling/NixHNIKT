bc9a105df114408f0b076bf2dc71819950dabd75
jhhhnikt <jan.henrik.hasselberg@hnikt.no>
2024-06-19 15:34:23 +0200

    Oppdatert fvm-install skript.

--- home/cli/fvm-install.nix
+++ home/cli/fvm-install.nix
@@ -2,9 +2,22 @@
   git_commit = "b42228b2523d04206aa8b50e582c89f33c24713a";
 in
   pkgs.writeShellScriptBin "fvm-install" ''
-    if ! command -v fvm &> /dev/null; then
-      curl -fsSL https://raw.githubusercontent.com/leoafarias/fvm/${git_commit}/scripts/install.sh | bash
-    else
-      echo fvm already installed.
+    git_commit="${git_commit}"
+    fvm_folder="~/.fvm_flutter/bin"
+    fvm_executable="$fvm_folder/fvm"
+
+    if [[ ! -x "$fvm_executable" ]]; then
+        tmp=$(mktemp)
+        curl -fsSL https://raw.githubusercontent.com/leoafarias/fvm/$git_commit/scripts/install.sh | bash 2> $tmp
+        if cat $tmp | grep "error: Failed to create symlink."; then
+            echo "Could not create symlink but this is expected."
+        else
+            cat $tmp
+        fi
+        rm $tmp
     fi
+
+    echo
+    echo "Add $fvm_folder to your PATH to complete setup."
+    echo "Or run $fvm_executable directly."
   ''
