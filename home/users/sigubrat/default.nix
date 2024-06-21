735ce3e99a66ee9c1c37697a24e0c86af31e0e5b
Sigurd Brattland <47779602+sigubrat@users.noreply.github.com>
2024-06-21 08:54:55 +0200

    Fix git not working in vscode terminal by copying ssh config instead of symlinking it (#117)

--- home/users/sigubrat/default.nix
+++ home/users/sigubrat/default.nix
@@ -9,6 +9,13 @@
     userName = "sigubrat";
   };
 
+  # Fix git not working in vscode terminal
+  # Copies the ssh config instead of symlinking it
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
+  
   dconf.settings = {
     # Keybindings
     "org/gnome/settings-daemon/plugins/media-keys" = {
