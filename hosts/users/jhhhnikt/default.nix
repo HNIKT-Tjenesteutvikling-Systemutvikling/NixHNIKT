b7484a06333b4ea5907bb2125c8c708484fe4dee
jhhhnikt <jan.henrik.hasselberg@hnikt.no>
2024-06-22 21:32:48 +0200

    jhhhnikt, oppdatert theme og host-file.

--- hosts/users/jhhhnikt/default.nix
+++ hosts/users/jhhhnikt/default.nix
@@ -9,12 +9,20 @@
   ];
   desktop.environment = "gnome";
   networking.hostName = "jhhhnikt";
+  networking.extraHosts = ''
+    # Local ipv4 dev.
+    127.0.0.1 app.localhost
+    127.0.0.1 keycloak.localhost
+    # Local ipv6 dev.
+    ::1 app.localhost
+    ::1 keycloak.localhost
+  '';
   services.xserver = {
-    videoDrivers = ["intel" "displaylink"]; # Optional use displayLink for USB-C docking station
+    videoDrivers = ["intel"]; # Optional use displayLink for USB-C docking station, e.g. ["intel" "displaylink"];
     displayManager = {
+      # Optional command xrandr, e.g. ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-9 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-14 --mode 2560x1440 --pos 4480x0 --rotate normal
       sessionCommands = ''
         ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-        ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-9 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-14 --mode 2560x1440 --pos 4480x0 --rotate normal
       '';
     };
   };
