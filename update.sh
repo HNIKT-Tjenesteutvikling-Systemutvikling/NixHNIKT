b7d77c1f5dfe8a27f3cd56a3e762f8ac678ca10a
merrinx <gako358@outlook.com>
2024-06-23 22:31:22 +0200

    Remove prints

--- update.sh
+++ update.sh
@@ -48,6 +48,7 @@ else
         git pull > /dev/null 2>&1
     else
         echo "No changes detected in the remote repository, exiting."
+        echo "Nothing to update..."
         exit 0
     fi
 fi
@@ -77,10 +78,8 @@ sudo -v
 (sudo nixos-rebuild switch --flake .#$flake &>nixos-switch.log || (cat nixos-switch.log | grep --color error && echo "An error occurred during the rebuild. Do you want to continue? (yes/no)" && read continue && if [[ "$continue" == "no" ]]; then exit 1; fi)) &
 spinner $! "System updating..."
 echo ""
-echo ""
 
 # Update home-manager
-echo "System updated!, updating home-manager for $home_name..."
 (home-manager switch --flake .#$home_name &>home-manager.log || (cat home-manager.log | grep --color error && echo "An error occurred during the home-manager update. Exiting." && exit 1)) &
 spinner $! "Updating home"
 echo ""
