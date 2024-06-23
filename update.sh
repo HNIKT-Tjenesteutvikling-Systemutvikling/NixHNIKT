892c8a2e5ca1fd19db9ed79e955aa50ab2999b70
merrinx <gako358@outlook.com>
2024-06-23 18:55:17 +0200

    Rewrite of update script, no user validation, better error handling

--- update.sh
+++ update.sh
@@ -1,72 +1,56 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
+
+# Fetch the latest changes from the remote repository
+git fetch
+
+# Check if there are any changes between the local and remote repository
+if git diff --quiet HEAD origin/master && git diff --quiet '*.nix'; then
+    echo "No changes detected, exiting."
+    exit 0
+fi
+
+# If there are changes, pull the latest changes from the remote repository
+git pull > /dev/null 2>&1
 
 # Extract the username from the userSetting.nix file
 flake=$(hostname)
 home_name="dev@$flake"
 
-# Test system
-clear
-echo "Do you want to test the system or home-manager?"
-echo "1. System, 2. Home-manager, 3. Both, 4. None"
-read test
+# Check disk space
+disk_space=$(df /dev/nvme0n1p1 | awk 'NR==2 {print $5}' | sed 's/%//g')
 
-if [[ "$test" == "1" ]]; then
-    nixos-rebuild build --flake .#$flake
-    exit 0
-elif [[ "$test" == "2" ]]; then
-    home-manager build --flake .#$home_name
-    exit 0
-elif [[ "$test" == "3" ]]; then
-    nixos-rebuild build --flake .#$flake
-    home-manager build --flake .#$home_name
-    exit 0
-else
-    continue
-fi
-
-# nix-collect-garbage
-
-echo "Updating system..."
-echo "Run a garbage collection?"
-echo "Used to clean up old generations"
-echo "Not needed if you have enough space"
-echo "1. Yes, 2. No"
-read gc
-
-# Run a garbage collection
-if [[ "$gc" == "1" ]]; then
+# Run a garbage collection if disk space is less than 60%
+echo "Checking disk space..."
+if (( disk_space > 60 )); then
+    echo "Disk space is $disk_space% "
+    echo "which is more than 60%, running garbage collection,"
+    echo "to free up boot..."
     sudo nix-collect-garbage -d
-    continue
 else
-    continue
+    echo "Enough disk space available, skipping garbage collection..."
+    echo "Deleting older generations..."
+    nix-collect-garbage --delete-older-than 28d
+    home-manager expire-generations "-19 days"
 fi
-
-# delete older generations
-nix-collect-garbage --delete-older-than 28d
-
-git pull
-clear
 
 # Run the nixos-rebuild command
 echo "Updating system for $flake..."
-sudo nixos-rebuild switch --flake .#$flake
-
-# Clean up
-clear
-
-home-manager expire-generations "-19 days"
+sudo nixos-rebuild switch --flake .#$flake &>nixos-switch.log || (cat nixos-switch.log | grep --color error && echo "An error occurred during the rebuild. Do you want to continue? (yes/no)" && read continue && if [[ "$continue" == "no" ]]; then exit 1; fi)
 
 # Update home-manager
 echo "System updated!, updating home-manager for $home_name..."
-home-manager switch --flake .#$home_name
+home-manager switch --flake .#$home_name &>home-manager.log || (cat home-manager.log | grep --color error && echo "An error occurred during the home-manager update. Exiting." && exit 1)
+
+# Check if there were any errors during the execution of the script
+if ! grep -q "error" nixos-switch.log && ! grep -q "error" home-manager.log; then
+    rm nixos-switch.log home-manager.log
+fi
 
 echo ""
-echo "System and home-manager updated!"
 echo "Some updates may require a restart"
 echo "Do you want to restart now?"
-
 echo "1. Yes, 2. No"
 read restart
 
