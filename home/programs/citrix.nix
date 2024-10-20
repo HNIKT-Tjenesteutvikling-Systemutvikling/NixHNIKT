4b6564c3e8a9ab96990d2466f3853c63824759e1
merrinx <gako358@outlook.com>
2024-10-20 11:45:50 +0200

    Bump citrix to 24.05

--- home/programs/citrix.nix
+++ home/programs/citrix.nix
@@ -11,7 +11,7 @@ with builtins; let
   extraCerts = [];
 
   citrixOverlay = self: super: {
-    citrix_workspace_23_09_0 = super.citrix_workspace_23_09_0.overrideAttrs (oldAttrs: rec {
+    citrix_workspace_24_05_0 = super.citrix_workspace_24_05_0.overrideAttrs (oldAttrs: rec {
       inherit extraCerts;
       buildInputs = oldAttrs.buildInputs ++ [self.openssl];
       postInstall = ''
@@ -32,7 +32,7 @@ in {
 
     nixpkgs.overlays = [citrixOverlay];
     home.packages = with pkgs; [
-      citrix_workspace_23_09_0
+      citrix_workspace_24_05_0
     ];
   };
 }
