cfe35d0eadd94206c1fcbcc93aaa088e3860ca9d
merrinx <gako358@outlook.com>
2025-03-08 21:50:57 +0100

    Setup luks crypt

--- system/hardware/boot.nix
+++ system/hardware/boot.nix
@@ -16,6 +16,15 @@
     initrd = {
       availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
       kernelModules = [ ];
+      luks.devices = {
+        cryptroot = {
+          device = "/dev/nvme0n1p3";
+          preLVM = true;
+        };
+        cryptswap = {
+          device = "/dev/nvme0n1p2";
+        };
+      };
     };
     kernelModules = [
       "kvm-intel"
