936588df17496681f65fde37dfe25e109419125c
merrinx <gako358@outlook.com>
2025-03-13 09:12:08 +0100

    Setup cachix with action

---
+++ system/programs/cachix/hnikt.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    settings.substituters = [
+      "https://hnikt.cachix.org"
+    ];
+    settings.trusted-public-keys = [
+      "hnikt.cachix.org-1:68VdsP/kBPkwqQJA3UKGR6iy8pCAflsj5ekkbWr5LnU="
+    ];
+  };
+}
--- system/programs/default.nix
+++ system/programs/default.nix
@@ -16,6 +16,7 @@ with lib; {
   ];
 
   imports = [
+    ./cachix
     ./desktop.nix
     ./docker.nix
     ./fonts.nix
