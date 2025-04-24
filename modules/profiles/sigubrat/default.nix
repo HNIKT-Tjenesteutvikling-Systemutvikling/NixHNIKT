1307392c4893332e0e879e2e35f8c3f79afd95ca
Knut Oien <knut.andre.gulseth.oien@hnikt.no>
2025-04-24 14:09:57 +0200

    Wrapped git fix in conditional, if file exists

--- modules/profiles/sigubrat/default.nix
+++ modules/profiles/sigubrat/default.nix
@@ -1,3 +1,11 @@
+{ config
+, lib
+, ...
+}:
+let
+  sshConfigSourcePath = config.home.homeDirectory + "/.ssh/config_source";
+  sourceFileExists = builtins.pathExists sshConfigSourcePath;
+in
 {
   imports = [ ../../default.nix ];
 
@@ -9,7 +17,7 @@
 
   # Fix git not working in vscode terminal
   # Copies the ssh config instead of symlinking it
-  home.file.".ssh/config" = {
+  home.file.".ssh/config" = lib.mkIf sourceFileExists {
     target = ".ssh/config_source";
     onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
   };
