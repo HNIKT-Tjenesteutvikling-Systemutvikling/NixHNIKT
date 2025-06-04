419483c75af5dcfe42eb87373ffaa5f5d87b9e70
merrinx <gako.footwork856@passinbox.com>
2025-06-04 13:54:24 +0200

    Fix intellij...

--- modules/programs/intellij.nix
+++ modules/programs/intellij.nix
@@ -19,12 +19,19 @@ let
 
   entries = lib.mapAttrsToList mkEntry devSDKs;
   devSymlink = pkgs.linkFarm "local-dev" entries;
+
+  intellijWrapper = pkgs.writeShellScriptBin "idiotitor" ''
+    exec ${pkgs.jetbrains.idea-ultimate}/bin/idea-ultimate "$@"
+  '';
 in
 {
   options.program.intellij.enable = lib.mkEnableOption "intellij";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.jetbrains.idea-ultimate ];
+    home.packages = [
+      pkgs.jetbrains.idea-ultimate
+      intellijWrapper
+    ];
     home.file.".local/dev".source = devSymlink;
   };
 }
