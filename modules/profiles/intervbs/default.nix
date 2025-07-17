cacd101bb02ec398dcb351a28d8486f0c3a162d4
merrinx <gako.footwork856@passinbox.com>
2025-07-17 14:22:38 +0200

    Moved ssh symlinking from users to git.nix

--- modules/profiles/intervbs/default.nix
+++ modules/profiles/intervbs/default.nix
@@ -1,11 +1,3 @@
-{ config
-, lib
-, ...
-}:
-let
-  sshConfigSourcePath = config.home.homeDirectory + "/.ssh/config_source";
-  sourceFileExists = builtins.pathExists sshConfigSourcePath;
-in
 {
   imports = [ ../../default.nix ];
 
@@ -13,13 +5,6 @@ in
     enable = true;
     userEmail = "joran@lillegaard.com";
     userName = "intervbs";
-  };
-
-  # Fix git not working in vscode terminal
-  # Copies the ssh config instead of symlinking it
-  home.file.".ssh/config" = lib.mkIf sourceFileExists {
-    target = ".ssh/config_source";
-    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
   };
 
   # Home modules to load
--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ pkgs
+, config
+, lib
+, ...
+}:
 let
   gitConfig = {
     core = {
@@ -21,14 +25,26 @@ let
       "ssh://git@github.com".pushInsteadOf = "gh:";
     };
   };
+
+  sshConfigSourcePath = config.home.homeDirectory + "/.ssh/config_source";
+  sourceFileExists = builtins.pathExists sshConfigSourcePath;
 in
 {
-  home.packages = with pkgs.gitAndTools; [
-    diff-so-fancy # git diff with colors
-    git-crypt # git files encryption
-    hub # github command-line client
-    tig # diff and commit view
-  ];
+  home = {
+    packages = with pkgs.gitAndTools; [
+      diff-so-fancy # git diff with colors
+      git-crypt # git files encryption
+      hub # github command-line client
+      tig # diff and commit view
+    ];
+
+    # Fix git not working in vscode terminal
+    # Copies the ssh config instead of symlinking it
+    file.".ssh/config" = lib.mkIf sourceFileExists {
+      target = ".ssh/config_source";
+      onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+    };
+  };
 
   programs.git = {
     enable = true;
