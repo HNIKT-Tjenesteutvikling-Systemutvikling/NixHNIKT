fb935f9b2b100bbb6e9e5de3f0964780b9404112
merrinx <gako.footwork856@passinbox.com>
2025-07-22 19:02:30 +0200

    Add gnome portal for gnome config

--- system/services/gnome.nix
+++ system/services/gnome.nix
@@ -50,6 +50,7 @@
         extraPortals = with pkgs; [
           xdg-desktop-portal-wlr
           xdg-desktop-portal-gtk
+          xdg-desktop-portal-gnome
         ];
       };
       environment.gnome.excludePackages = with pkgs; [
