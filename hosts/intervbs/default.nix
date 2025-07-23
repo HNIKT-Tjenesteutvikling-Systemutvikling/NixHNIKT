44e881720ada711af52d2ca9b6fd170f5257cb6b
merrinx <gako.footwork856@passinbox.com>
2025-07-23 11:32:53 +0200

    Test debug dbus update command

--- hosts/intervbs/default.nix
+++ hosts/intervbs/default.nix
@@ -2,14 +2,14 @@
 {
   networking.hostName = "intervbs";
   users.users.dev.initialHashedPassword = "$7$CU..../..../9Xx8NRHkSAWJioosG3LS1$luAa3BlGiM28xWgJOH2XEXmQYuP58qvpjtsEU21im07";
-  services.xserver = {
-    videoDrivers = [ "modesetting" ]; # Optional use displayLink for USB-C docking station
-    displayManager = {
-      sessionCommands = ''
-        ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-      '';
-    };
-  };
+  # services.xserver = {
+  #   videoDrivers = [ "modesetting" ]; # Optional use displayLink for USB-C docking station
+  #   displayManager = {
+  #     sessionCommands = ''
+  #       ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
+  #     '';
+  #   };
+  # };
 
   # Modules loaded
   environment.desktop.windowManager = "gnome";
