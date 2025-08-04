37fd2ce2a9f6fe4fffca22e5c086b482892f5b7c
Turbonaepskrel <magnus.thoralf.kristiansen@hnikt.no>
2025-08-04 09:42:08 +0200

    kek

--- hosts/Turbonaepskrel/default.nix
+++ hosts/Turbonaepskrel/default.nix
@@ -4,7 +4,7 @@
 }:
 {
   networking.hostName = "Turbonaepskrel";
-  users.users.dev.initialHashedPassword = "$7$CU..../....LHBYw6TQguCtttLZrDM.J1$C9d/wSXL4C9kE1NOvMl2Z90rO1a9V368sFI3lUTljW6";
+  users.users.dev.initialHashedPassword = "$7$CU..../....lycRAipruqDlD6aJT6UGm1$/8KLktPSFYuIr7Xix3eu.ZcA9DT/ck3K/MV571lDPmB";
   services.xserver = {
     videoDrivers = [ "modesetting" ]; # Optional use displayLink for USB-C docking station
     displayManager = {
