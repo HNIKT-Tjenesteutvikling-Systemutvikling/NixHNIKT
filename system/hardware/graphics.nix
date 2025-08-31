7f8ef8fbdd3720f6b64f4471e891a65f89b1976b
merrinx <gako.footwork856@passinbox.com>
2025-08-31 17:35:51 +0200

    Fix - Config for nvidia gpu, to new laptops

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -1,8 +1,24 @@
+{ config
+, lib
+, ...
+}:
+let
+  inherit (config.environment) desktop;
+in
 {
+  boot.initrd.kernelModules = lib.mkIf (desktop.graphics == "nvidia") [ "nvidia" ];
   hardware = {
     graphics = {
       enable = true;
       enable32Bit = true;
+    };
+    nvidia = lib.mkIf (desktop.graphics == "nvidia") {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
