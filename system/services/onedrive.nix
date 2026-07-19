39d22b403d0ea5fbce796762e1c484a8ab72c459
merrinx <gako.footwork856@passinbox.com>
2026-07-19 21:38:36 +0200

    feat(onedrive): add systemd user service and reauth script for onedrive

--- system/services/onedrive.nix
+++ system/services/onedrive.nix
@@ -1,16 +1,35 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.service.onedrive;
+  onedrivePkg = config.services.onedrive.package;
 in
 {
   options.service.onedrive.enable = lib.mkEnableOption "onedrive";
 
   config = lib.mkIf cfg.enable {
     services.onedrive.enable = true;
+
+    systemd.user.services."onedrive@" = {
+      serviceConfig = {
+        ExecStart = lib.mkForce ''
+          ${onedrivePkg}/bin/onedrive --monitor --disable-notifications --confdir=%h/.config/%i
+        '';
+        RestartSec = lib.mkForce 60;
+      };
+    };
+
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "onedrive-reauth" ''
+        systemctl --user stop onedrive@onedrive
+        ${onedrivePkg}/bin/onedrive --reauth
+        exec systemctl --user start onedrive@onedrive
+      '')
+    ];
     environment.persistence."/persist" = {
       users.dev = {
         directories = [
